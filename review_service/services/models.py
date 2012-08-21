dd29c85951dd8ee9bc30d535bc9646a36b34b51b
vladlubenskiy <vlad.lubenskiy@gmail.com>
2012-08-21 23:53:34 +0300

    Added model for Author

--- review_service/services/models.py
+++ review_service/services/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
-class Publisher(models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=30)
-    address = models.CharField(max_length=50)
-    city = models.CharField(max_length=60)
-    state_province = models.CharField(max_length=30)
-    country = models.CharField(max_length=50)
-    website = models.URLField()
+    email = models.EmailField()
+    locked = models.BooleanField()
